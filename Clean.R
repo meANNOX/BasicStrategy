29081cd386363c551b021ce2dc8baedb3fa393ee
meANNOX <me@annox.com>
2017-08-14 16:12:20 +0200

    Dummy text for initial files
    Fixing load and clean

--- Clean.R
+++ Clean.R
@@ -1,1 +1,4 @@
 ## Clean ##
+
+#This is where all the ugly stuff lives - 
+#taking care of missing values, merging data frames, handling outliers.
--- Load.R
+++ Load.R
@@ -1,3 +1,11 @@
 ## Load ##
 
-# change #
+#Takes care of loading in all the data required. 
+#Typically this is a short file, reading in data from files, URLs and/or ODBC.
+#Depending on the project at this point I'll either write out the workspace
+#using save() or just keep things in memory for the next step.
+
+## laod in packages (quant)
+
+
+## load in timeseries (SANDP)
