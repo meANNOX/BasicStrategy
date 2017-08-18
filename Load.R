129e68dca7c64343aa3369f7dc163295c548bf1c
meANNOX <me@annox.com>
2017-08-18 14:26:40 +0200

    futher development

--- Load.R
+++ Load.R
@@ -11,6 +11,10 @@ install.packages("quantmod")
 install.packages("jsonlite")
 install.packages("Quandl")
 install.packages("ggplot2")
+install.packages("caTools")
+install.packages("data.table")
+install.packages("TTR")
+
 #   ________________________________________________________
 #   Load in data                                            ####
 # this is a level 1 section. l1 sections are are high-level 
@@ -19,7 +23,9 @@ library("jsonlite")
 library("quantmod")
 library(Quandl)
 library("ggplot2")
-
+library("caTools")
+library("data.table")
+library("TTR")
 
 Quandl.api_key("7z5ifJjJ4baqE2u5eyg2")
 # set input data
@@ -29,22 +35,48 @@ end_date <- "2013-01-01"
 
 # get list of instrument uids
 
-
-#
-
-
 # Retrieve timeseries from instrument uics
 mydata <- Quandl("OPEC/ORB", start_date="2003-01-01", end_date = "2013-01-01")
-data <- Quandl("OPEC/ORB", , start_date="2003-01-01", end_date = "2013-01-01", 
-               collapse = "quarterly", type = "zoo", limit = 3)
+data <- Quandl("OPEC/ORB", start_date="2003-01-01", end_date = "2013-01-01", 
+               collapse = "daily", type = "raw")
+
+data <- as.data.table(data)
+
+
+## do moving average
+
+plot(data)
+
+
+## or alternatively stationize series and do a version of ar
+
+## 
+
+plot(diff(data$Value))
+
+#and do ar process on this, perhaps with longer lags, like -1, -2 ... up to -10.
+
+
+
+
+datahead(data)
+tail(data)
+describe(data)
+summary(data)
+
+class(data)
+str(data)
+plot(data)
+
 
 
 ## load in various time series, and etf should have +5 years data, weekly
 
-## do a forecast at a time ## perhaps for all timeseries simultaniously?
 
-## use optimizations
 
+## do a forecast at a time ## perhaps for all timeseries simultaniously
+
+## just do ewmac signal with running window perhaps.
 
 ## load in timeseries (S&P)
 
